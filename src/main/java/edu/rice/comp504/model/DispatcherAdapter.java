79df62e87da0eaea41e8e35fdad57da01a236561
ManuMaheshwari <maheshwarimanu11@gmail.com>
2018-11-20 11:32:23 -0600

    First commit

---
+++ src/main/java/edu/rice/comp504/model/DispatcherAdapter.java
@@ -0,0 +1,197 @@
+package edu.rice.comp504.model;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import edu.rice.comp504.model.obj.ChatRoom;
+import edu.rice.comp504.model.obj.Message;
+import edu.rice.comp504.model.obj.User;
+import edu.rice.comp504.model.res.*;
+
+public class DispatcherAdapter extends Observable {
+
+    private int nextUserId;
+    private int nextRoomId;
+    private int nextMessageId;
+
+    // Maps user id to the user
+    private Map<Integer, User> users;
+
+    // Maps room id to the chat room
+    private Map<Integer, ChatRoom> rooms;
+
+    // Maps message id to the message
+    private Map<Integer, Message> messages;
+
+    // Maps session to user id
+    private Map<Session, Integer> userIdFromSession;
+
+    /**
+     * Constructor, initializing all private fields.
+     */
+    public DispatcherAdapter() {
+        this.nextRoomId = 0;
+        this.nextUserId = 0;
+        this.nextMessageId = 0;
+        this.users = new ConcurrentHashMap<>();
+        this.rooms = new ConcurrentHashMap<>();
+        this.messages = new ConcurrentHashMap<>();
+        this.userIdFromSession = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Allocate a user id for a new session.
+     * @param session the new session
+     */
+    public void newSession(Session session) {
+
+    }
+
+    /**
+     * Get the user if from a session.
+     * @param session the session
+     * @return the user id binding with session
+     */
+    public int getUserIdFromSession(Session session) {
+        return this.userIdFromSession.get(session);
+    }
+
+    /**
+     * Determine whether the session exists.
+     * @param session the session
+     * @return whether the session is still connected or not
+     */
+    public boolean containsSession(Session session) {
+        return this.userIdFromSession.containsKey(session);
+    }
+
+    /**
+     * Load a user into the environment.
+     * @param session the session that requests to called the method
+     * @param body of format "name age location school"
+     * @return the new user that has been loaded
+     */
+    public User loadUser(Session session, String body) {
+        return null;
+    }
+
+    /**
+     * Load a room into the environment.
+     * @param session the session that requests to called the method
+     * @param body of format "name ageLower ageUpper {[location],}*{[location]} {[school],}*{[school]}"
+     * @return the new room that has been loaded
+     */
+    public ChatRoom loadRoom(Session session, String body) {
+        return null;
+    }
+
+    /**
+     * Remove a user with given userId from the environment.
+     * @param userId the id of the user to be removed
+     */
+    public void unloadUser(int userId) {
+
+    }
+
+    /**
+     * Remove a room with given roomId from the environment.
+     * @param roomId the id of the chat room to be removed
+     */
+    public void unloadRoom(int roomId) {
+
+    }
+
+    /**
+     * Make a user join a chat room.
+     * @param session the session that requests to called the method
+     * @param body of format "roomId"
+     */
+    public void joinRoom(Session session, String body) {
+
+    }
+
+    /**
+     * Make a user volunteer to leave a chat room.
+     * @param session the session that requests to called the method
+     * @param body of format "roomId"
+     */
+    public void leaveRoom(Session session, String body) {
+
+    }
+
+    /**
+     * Make modification on chat room filer by the owner.
+     * @param session the session of the chat room owner
+     * @param body of format "roomId lower upper {[location],}*{[location]} {[school],}*{[school]}"
+     */
+    public void modifyRoom(Session session, String body) {
+
+    }
+
+    /**
+     * A sender sends a string message to a receiver.
+     * @param session the session of the message sender
+     * @param body of format "roomId receiverId rawMessage"
+     */
+    public void sendMessage(Session session, String body) {
+
+    }
+
+    /**
+     * Acknowledge the message from the receiver.
+     * @param session the session of the message receiver
+     * @param body of format "msgId"
+     */
+    public void ackMessage(Session session, String body) {
+
+    }
+
+    /**
+     * Send query result from controller to front end.
+     * @param session the session that requests to called the method
+     * @param body of format "type roomId [senderId] [receiverId]"
+     */
+    public void query(Session session, String body) {
+
+    }
+
+    /**
+     * Notify the client for refreshing.
+     * @param user user expected to receive the notification
+     * @param response the information for notifying
+     */
+    public void notifyClient(User user, AResponse response) {
+
+    }
+
+    /**
+     * Get the names of all chat room members.
+     * @param roomId the id of the chat room
+     * @return all chat room members, mapping from user id to user name
+     */
+    private Map<Integer, String> getUsers(int roomId) {
+        return null;
+    }
+
+    /**
+     * Get notifications in the chat room.
+     * @param roomId the id of the chat room
+     * @return notifications of the chat room
+     */
+    private List<String> getNotifications(int roomId) {
+        return null;
+    }
+
+    /**
+     * Get chat history between user A and user B (commutative).
+     * @param roomId the id of the chat room
+     * @param userAId the id of user A
+     * @param userBId the id of user B
+     * @return chat history between user A and user B at a chat room
+     */
+    private List<Message> getChatHistory(int roomId, int userAId, int userBId) {
+        return null;
+    }
+}
---
+++ src/main/java/edu/rice/comp504/model/obj/User.java
@@ -0,0 +1,156 @@
+package edu.rice.comp504.model.obj;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.*;
+import java.util.Observable;
+import java.util.Observer;
+/*
+The User class defines a user object and private fields of a user
+*/
+public class User implements Observer {
+
+    private int id;
+    private transient Session session;
+
+    private String name;
+    private int age;
+    private String location;
+    private String school;
+
+    private List<Integer> joinedRoomIds;
+    private List<Integer> availableRoomIds;
+
+    /**
+     * Constructor.
+     * @param id the user id
+     * @param session the user session
+     * @param name the user name when register
+     * @param age the user age when register
+     * @param location the user location when register
+     * @param school the user school when register
+     * @param rooms the ChatRoom array contains all rooms
+     */
+    public User(int id, Session session, String name, int age,
+                String location, String school, ChatRoom[] rooms) {
+        this.id = id;
+        this.session = session;
+
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.school = school;
+
+        this.joinedRoomIds = new LinkedList<>();
+        this.availableRoomIds = new LinkedList<>();
+
+        for (ChatRoom room : rooms) {
+            this.availableRoomIds.add(room.getId());
+        }
+    }
+
+    /**
+     * Get current user id
+     * @return the user id
+     * */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Get current user session
+     * @return the user session
+     * */
+    public Session getSession() {
+        return this.session;
+    }
+
+    /**
+     * Get the user name
+     * @return the user name
+     * */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Get the user age
+     * @return the user age
+     * */
+    public int getAge() {
+        return this.age;
+    }
+
+    /**
+     * Get the user location
+     * @return the user register location in String
+     * */
+    public String getLocation() {
+        return this.location;
+    }
+
+    /**
+     * Get the user school
+     * @return the user register school in String
+     * */
+    public String getSchool() {
+        return this.school;
+    }
+
+    /**
+     * Get a list of user joined chat rooms
+     * @return joined rooms ids
+     * */
+    public List<Integer> getJoinedRoomIds() {
+        return this.joinedRoomIds;
+    }
+
+    /**
+     * Get a list of user available chat rooms
+     * @return available chat rooms ids
+     * */
+    public List<Integer> getAvailableRoomIds() {
+        return this.availableRoomIds;
+    }
+
+    /**
+     * Get a chat room id then store into available chat room list
+     * @param room the chat room object
+     * */
+    public void addRoom(ChatRoom room) {
+
+    }
+
+    /**
+     * Get a chat room id then remove it from both user joined rooms list and available rooms list
+     * @param room the chat room object
+     * */
+    public void removeRoom(ChatRoom room) {
+
+    }
+
+    /**
+     * Move a chat room from available room list to joined room list
+     * @param room the chat room object
+     * */
+    public void moveToJoined(ChatRoom room) {
+
+    }
+
+    /**
+     * Move a chat room from joined room list to available room list
+     * @param room the chat room object
+     * */
+    public void moveToAvailable(ChatRoom room) {
+
+    }
+
+    /**
+     * User update when observable has changed
+     * */
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+}
