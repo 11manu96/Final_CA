4aab415f36cf04b2ea96dbd4116c74ef67e927bb
Jonathan Wang <jonathanwang017@berkeley.edu>
2018-11-21 10:51:27 -0600

    add starter js code

--- src/main/resources/public/js/chatapp.js
+++ src/main/resources/public/js/chatapp.js
@@ -6,21 +6,79 @@ const webSocket = new WebSocket("ws://" + location.hostname + ":" + location.por
  * Entry point into chat room
  */
 window.onload = function() {
+    $("#btn-login").onclick(logIn);
+    $("#btn-enter").onclick(enterRoom);
+    $("#btn-exit").onclick(exitRoom);
+    $("#btn-exit-all").onclick(exitAllRooms);
+    $("#btn-join").onclick(joinRoom);
+    $("#btn-create").onclick(createRoom);
+    $("#btn-send").onclick(sendMessage);
+
+    webSocket.onmessage = function(message) {
+        updateChatApp(message);
+    };
+}
+
+/**
+ * Send request to websocket to create user
+ */
+function logIn() {
+    var userName = $("#user-name").val();
+    var userAge = $("#user-age").val();
+    var userLocation = $("#user-location").val();
+    var userSchool = $("#user-school").val();
+}
+
+/**
+ * Send request to websocket to enter room
+ */
+function enterRoom() {
+    var selectedRoom = $("#slt-joined-rooms").val();
+}
+
+/**
+ * Send request to websocket to exit all rooms
+ */
+function exitRoom() {
+    var selectedRoom = $("#slt-joined-rooms").val();
+}
+
+/**
+ * Send request to websocket to exit all rooms
+ */
+function exitAllRooms() {
 
 }
 
 /**
- * Send a message to the server.
- * @param msg  The message to send to the server.
+ * Send request to websocket to join room
  */
-function sendMessage(msg) {
-
+function joinRoom() {
+    var selectedRoom = $("#slt-available-rooms").val();
 }
 
 /**
- * Update the chat room with a message.
- * @param message  The message to update the chat room with.
+ * Send request to websocket to create room
  */
-function updateChatRoom(message) {
+function createRoom() {
+    var roomName = $("#room-name").val();
+    var roomMinAge = $("#room-min-age").val();
+    var roomMaxAge = $("#room-max-age").val();
+    var roomLocations = $("#slt-room-location").val();
+    var roomSchools = $("#slt-room-school").val();
+}
 
+/**
+ * Send request to websocket to send message
+ */
+function sendMessage() {
+    var message = $("#chat-message").val();
+}
+
+/**
+ * Receive data from websocket to update view
+ * @param message data from websocket
+ */
+function updateChatApp(message) {
+    // parse message to determine how to update view
 }
