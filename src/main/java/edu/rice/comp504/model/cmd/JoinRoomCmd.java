61091b2ac812dabda4a6914aa19d4a0212a03dc2
Yanjun Yang <yj.yang1214@gmail.com>
2018-11-21 22:45:19 -0600

    first commit for the JoinRoomCmd

--- src/main/java/edu/rice/comp504/model/cmd/JoinRoomCmd.java
+++ src/main/java/edu/rice/comp504/model/cmd/JoinRoomCmd.java
@@ -9,36 +9,68 @@ public class JoinRoomCmd implements IUserCmd{
     private User user;
     private static JoinRoomCmd instance;
 
+    /**
+     * Constructor.
+     * @param chatRoom chatRoom
+     * @param user user
+     */
     private JoinRoomCmd(ChatRoom chatRoom, User user){
         this.chatRoom = chatRoom;
         this.user = user;
     }
 
+    /**
+     * Return to a JoinRoomCmd instance.
+     * @param chatRoom chatRoom
+     * @param user user
+     * @return a JoinRoomCmd
+     */
     public static JoinRoomCmd makeCmd(ChatRoom chatRoom, User user){
         if (instance == null){
             instance = new JoinRoomCmd(chatRoom, user);
         } else {
-
+            instance.setChatRoom(chatRoom);
+            instance.setUser(user);
         }
         return instance;
     }
 
+    /**
+     * Helper function get user.
+     * @return user
+     */
     public User getUser(){
         return user;
     }
 
+    /**
+     * Helper function get chatRoom.
+     * @return chatRoom
+     */
     public ChatRoom getChatRoom(){
         return chatRoom;
     }
 
+    /**
+     * Helper function set chatRoom.
+     * @param chatRoom chatRoom
+     */
     public void setChatRoom(ChatRoom chatRoom){
         this.chatRoom = chatRoom;
     }
 
+    /**
+     * Helper function set user.
+     * @param user user
+     */
     public void setUser(User user){
         this.user = user;
     }
 
+    /**
+     * Execute the command.
+     * @param context user
+     */
     @Override
     public void execute(User context) {
         chatRoom.addUser(context);
